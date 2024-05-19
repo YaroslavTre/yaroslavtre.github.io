47116ddb22c6388a384be6f808ac3c73daa466d7
YaroslavTre <yarestr52@gmail.com>
2024-05-19 16:19:06 +0200

    index.html

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const swiperTop = new Swiper('.top__swiper', {
+  // Optional parameters
+  effect: 'fade',
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
+const swiper = new Swiper(".about__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
